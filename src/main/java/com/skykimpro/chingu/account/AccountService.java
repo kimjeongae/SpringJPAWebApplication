10eee00bcbfc0b75f9643910705463a1c83016b2
kjaa34@gmail.com <kjaa34@gmail.com>
2020-10-01 16:14:51 +0900

    When forget the password

--- src/main/java/com/skykimpro/chingu/account/AccountService.java
+++ src/main/java/com/skykimpro/chingu/account/AccountService.java
@@ -108,4 +108,13 @@ public class AccountService implements UserDetailsService {
         accountRepository.save(account); // merge
         login(account);
     }
+
+    public void sendLoginLink(Account account) {
+        account.generateEmailCheckToken();
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(account.getEmail());
+        mailMessage.setSubject("Chingu 로그인 링크");
+        mailMessage.setText("/login-by-email?token=" + account.getEmailCheckToken() + "&email="+account.getEmail());
+        javaMailSender.send(mailMessage);
+    }
 }
