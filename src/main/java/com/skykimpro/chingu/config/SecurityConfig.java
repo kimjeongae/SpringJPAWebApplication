1bd85a0da8bd67a942562f9c6984e2c39d47b9a6
kjaa34@gmail.com <kjaa34@gmail.com>
2020-11-22 17:21:53 +0900

    Study domain added.

--- src/main/java/com/skykimpro/chingu/config/SecurityConfig.java
+++ src/main/java/com/skykimpro/chingu/config/SecurityConfig.java
@@ -2,7 +2,6 @@ package com.skykimpro.chingu.config;
 
 import com.skykimpro.chingu.account.AccountService;
 import lombok.RequiredArgsConstructor;
-import org.hibernate.engine.jdbc.connections.internal.DatasourceConnectionProviderImpl;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
